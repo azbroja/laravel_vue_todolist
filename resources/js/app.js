129c7fabc711cc82db6dc64134401e659c6218b4
azbroja <azbroja@outlook.com>
2021-02-10 21:01:37 +0100

    add vue form with laravel api

---
+++ resources/js/app.js
@@ -0,0 +1,38 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import Vue from 'vue'
+
+//Main pages
+import App from './vue/app.vue'
+
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+const app = new Vue({
+    el: '#app',
+    components: { App }
+});
---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    List view
+  </div>
+</template>
+<script>
+
+
+export default {
+ 
+}
+</script>
