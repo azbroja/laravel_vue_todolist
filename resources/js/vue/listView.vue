f103f0c864642c6ceb452169c299e84a03d0d565
azbroja <azbroja@outlook.com>
2021-02-10 21:31:55 +0100

    show list of items

--- resources/js/vue/listView.vue
+++ resources/js/vue/listView.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-    List view
+    <div v-for="(item, index) in items" :key="index" >
+      <list-item :item="item" class="item" />
+    </div>
   </div>
 </template>
-<script>
 
+<script>
+import listItem from "./listItem"
 
 export default {
- 
+  props: ['items'],
+  components: { listItem }
+
 }
 </script>
+
+<style scoped>
+.item {
+  background: #e6e6e6;
+  padding: 5px;
+  margin-top: 5px;
+}
+</style>
